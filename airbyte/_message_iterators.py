bac91b1a0aaca94e0b903dcb6db42882479453a1
haglunddev <haglunddev@gmail.com>
2025-10-05 13:14:46 -0400

    Try quotes

--- airbyte/_message_iterators.py
+++ airbyte/_message_iterators.py
@@ -110,7 +110,7 @@ class AirbyteMessageIterator:
                             ),
                             # `meta` and `namespace` are not handled:
                             meta=None,
-                            # namespace=None,
+                            namespace="",
                         ),
                     )
 
